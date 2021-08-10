e934c2da0b67595e9f93c367ca6955cc9b95fdc5
Isabella Mighell <ikm4dm@virginia.edu>
2021-08-10 16:21:51 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+
+function mondayWork(work = "go to the office"){
+    return `This Monday, I will ${work}.`;
+}
+ 
+
+function wrapAdjective(visualFlair=`*`){
+    return function(adj="special"){ 
+        return `You are ${visualFlair}${adj}${visualFlair}!`
+    }
+}
